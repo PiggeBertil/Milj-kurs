0b45139234d5445fadafe1c7ce094ffa8ad0986f
EliasGuedra <90322377+EliasGuedra@users.noreply.github.com>
2025-04-25 09:25:55 +0200

    First assignment done

--- energisystemsprojekt.py
+++ energisystemsprojekt.py
@@ -15,25 +15,25 @@ eta = {'Wind' : 1, 'PV' : 1, 'Gas' : 0.4, 'Hydro' : 1, 'Battery' : 0.9} #you cou
 
 tech_data = {
     "Wind": {
-        "inv_cost": 1100,
+        "inv_cost": 1_100_000,  #euro/MW
         "run_cost": 0.1,
         "fuel_cost": 0,
         "lifetime": 25,
         "eff": None,
         "emission": 0,
-        "cap": {"SE": 280, "DK": 90, "DE": 180}
+        "cap": {"SE": 280_000, "DK": 90_000, "DE": 180_000}  #MW
     },
     "PV": {
-        "inv_cost": 600,
+        "inv_cost": 600_000,
         "run_cost": 0.1,
         "fuel_cost": 0,
         "lifetime": 25,
         "eff": None,
         "emission": 0,
-        "cap": {"SE": 75, "DK": 60, "DE": 460}
+        "cap": {"SE": 75_000, "DK": 60_000, "DE": 460_000}
     },
     "Gas": {
-        "inv_cost": 550,
+        "inv_cost": 550_000,
         "run_cost": 2,
         "fuel_cost": 22,
         "lifetime": 30,
@@ -48,10 +48,10 @@ tech_data = {
         "lifetime": 80,
         "eff": None,
         "emission": 0,
-        "cap": {"SE": 14, "DK": 0, "DE": 0}
+        "cap": {"SE": 14_000, "DK": 0, "DE": 0}
     },
     "Battery": {
-        "inv_cost": 150,
+        "inv_cost": 150_000,
         "run_cost": 0.1,
         "fuel_cost": 0,
         "lifetime": 10,
@@ -60,7 +60,7 @@ tech_data = {
         "cap": "inf"
     },
     "Transmission": {
-        "inv_cost": 2500,
+        "inv_cost": 2_500_000,
         "run_cost": 0,
         "fuel_cost": 0,
         "lifetime": 50,
@@ -69,7 +69,7 @@ tech_data = {
         "cap": "inf"
     },
     "Nuclear": {
-        "inv_cost": 7700,
+        "inv_cost": 7_700_000,
         "run_cost": 4,
         "fuel_cost": 3.2,
         "lifetime": 50,
@@ -78,9 +78,6 @@ tech_data = {
         "cap": "inf"
     }
 }
-
-#tech_data_df = pd.DataFrame.from_dict(tech_data, orient='index')
-
 
 discountrate = 0.05
 
@@ -126,8 +123,6 @@ def fuelData():
     for g in model.gens:
         fuel[g] = float(tech_data[g]['fuel_cost'])
     return fuel
-
-
 
 def lifetimeData():
     lifetime = {}
@@ -178,14 +173,28 @@ model.prod = Var(model.nodes, model.gens, model.hours, doc='Generator cap')
 def prodcapa_rule(model, nodes, gens, time):
     return model.prod[nodes, gens, time] <= model.capa[nodes, gens]
 
+def positive_rule(model, nodes, gens, time):
+    return model.prod[nodes, gens, time] >= 0
+
+
 def demand_satisfied_rule(model, n, t):
     #Ska vi gångra med eta?
     return sum(model.prod[n, g, t] for g in model.gens) >= model.demand[n, t]
 
 
+def windMax_rule(model, nodes, hours): # the wind can only blow so much
+    return model.prod[nodes, 'Wind', hours] <= model.wind[nodes, hours] * model.capa[nodes, 'Wind']
+
+def pvMax_rule(model, nodes, hours): # the sun can only shine so much
+    return model.prod[nodes, 'PV', hours] <= model.solar[nodes, hours] * model.capa[nodes, 'PV']
+
+
+model.prodWind = Constraint(model.nodes, model.hours, rule=windMax_rule)
+model.prodPV = Constraint(model.nodes, model.hours, rule=pvMax_rule)
 
 model.demandSatisfied = Constraint(model.nodes, model.hours, rule=demand_satisfied_rule)
 model.prodCapa = Constraint(model.nodes, model.gens, model.hours, rule=prodcapa_rule)
+model.positive = Constraint(model.nodes, model.gens, model.hours, rule=positive_rule)
 
 
 #OBJECTIVE FUNCTION
@@ -193,15 +202,23 @@ def objective_rule(model):
 
     cost = 0
     anual_cost = 0
+    running_cost = 0
+    fuel_cost = 0 
 
     for n in model.nodes:
         for g in model.gens:
             discount_factor = discountrate / (1 - 1/((1 + discountrate)**model.lifetime[g]))
             anual_cost += model.capa[n, g] * discount_factor * model.investment_cost[g]
 
+    for n in model.nodes:
+        for g in model.gens:
+            for h in model.hours:
+                running_cost += model.prod[n,g,h] * model.running_cost[g]
+                fuel_cost += model.prod[n,g,h] * model.fuel_cost[g] / model.eta[g]
+
 
     
-    return anual_cost
+    return anual_cost + running_cost + fuel_cost
 
 
 model.objective = Objective(rule=objective_rule, sense=minimize, doc='Objective function')
