3d8475526e4ee43f6626aaacca0c30436b9f1fb5
EliasGuedra <90322377+EliasGuedra@users.noreply.github.com>
2025-05-12 14:25:40 +0200

    sync

--- Miljö_project/Miljösimulering.py
+++ Miljö_project/Miljösimulering.py
@@ -81,10 +81,37 @@ plt.legend()
 plt.show()
 
 # Task 3
+
 A = np.array([0.113, 0.213, 0.258, 0.273, 0.1430])
 tau_0 = np.array([2.0, 12.2, 50.4, 243.3, np.inf])
 
+
+def I(t, U_cum):
+
+    k = 3.06 * 10 ** (-3)
+    summa = 0
+    for i in range(5):
+        tau_i = tau_0[i]*(1+k*U_cum)
+        summa += A[i] * np.exp(-t/tau_i)
+    return summa
+
+
+
+U = np.array([emisions_df.loc[emisions_df['Time (year)'] == t].values[0][1] for t in time])
+cumulative_emissions = np.cumsum(U)
+cumulative_emissions_shifted = np.concatenate(([0], cumulative_emissions[:-1])) # summan är upp till t-1
+
+
+plt.plot([I(t, cumulative_emissions_shifted[0])   for t in range(500)])
+plt.plot([I(t, cumulative_emissions_shifted[100]) for t in range(500)])
+plt.plot([I(t, cumulative_emissions_shifted[150]) for t in range(500)])
+plt.plot([I(t, cumulative_emissions_shifted[200]) for t in range(500)])
+plt.plot([I(t, cumulative_emissions_shifted[250]) for t in range(500)])
+
+plt.show()
+"""
 k = 3.06 * 10 ** (-3)
+
 
 U = np.array([emisions_df.loc[emisions_df['Time (year)'] == t].values[0][1] for t in time])
 cumulative_emissions = np.cumsum(U)
@@ -96,10 +123,11 @@ for i in range(5):
 
 Impulse = []
 for t in range(len(time)):
-    sum = 0
+    summa = 0
     for i in range(5):
-        sum += A[i] * np.exp(-t/tau_all[t][i])
-    Impulse.append(sum)
+        summa += A[i] * np.exp(-t/tau_all[t][i])
+    Impulse.append(summa)
 
-plt.plot(range(len(time)), Impulse)
+plt.plot(time, Impulse)
 plt.show()
+"""
