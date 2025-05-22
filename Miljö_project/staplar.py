49cab490c1488912d27ed6fbde73a4bb283a2a52
Vendela Sandell <sandellv@chalmers.se>
2025-05-22 21:21:35 +0200

    staplar

---
+++ Miljö_project/staplar.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# X-etiketter för grupper
+lambdas = ['λ = 0.5', 'λ = 0.8', 'λ = 1.3']
+
+# Data för varje case (valfria exempelvärden)
+case_1 = [1.86297, 2.23270, 2.64680]
+case_2 = [2.74276, 3.23911, 3.77840]
+case_3 = [3.22936, 3.79484, 4.40229]
+
+# X-positionsinställningar
+x = np.arange(len(lambdas))  # [0, 1, 2]
+width = 0.25  # Bredd på varje stapel
+
+# Skapa figur och staplar
+fig, ax = plt.subplots(figsize=(8, 5))
+bars1 = ax.bar(x - width, case_1, width, label='Case 1', color='#1f77b4')
+bars2 = ax.bar(x,         case_2, width, label='Case 2', color='#ff7f0e')
+bars3 = ax.bar(x + width, case_3, width, label='Case 3', color='#2ca02c')
+
+# Etiketter över varje stapel
+def add_labels(bars):
+    for bar in bars:
+        height = bar.get_height()
+        ax.annotate(f'{height}',
+                    xy=(bar.get_x() + bar.get_width()/2, height),
+                    xytext=(0, 3),
+                    textcoords="offset points",
+                    ha='center', va='bottom', fontsize=9)
+
+add_labels(bars1)
+add_labels(bars2)
+add_labels(bars3)
+
+# Layoutjustering
+ax.set_xlabel('Lambda')
+ax.set_ylabel('Värde')
+ax.set_title('Resultat per lambda och case')
+ax.set_xticks(x)
+ax.set_xticklabels(lambdas)
+ax.legend()
+
+plt.tight_layout()
+
+
+plt.show()
